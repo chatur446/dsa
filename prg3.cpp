e5f581d864c5857bf15d2e9c17d4e4f734d08a9e
chatur446 <ayushchaturvedi1305@gmail.com>
2025-03-13 21:46:50 -0700

    Add files via upload
    cpp code

---
+++ prg3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char c='a';
+    short int s = 37467;
+    int i = 845636586;
+    long int v = 85987297979275l;
+    long long int ii = 8975478798378976ll;
+
+    cout<<"char:"<<c<<endl;
+    cout<<"short int:"<<s<<endl;
+    cout<<"int:"<<i<<endl;
+    cout<<"long int:"<<v<<endl;
+    cout<<"long long int:"<<ii<<endl;
+     return 0;
+    
+}
